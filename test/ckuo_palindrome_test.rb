e6ec9e2aacc1b8dc5e5c0ce983cec520a05ad199
chaoobject001 <chao.kuo@gmail.com>
2021-12-19 21:56:51 -0800

    use refute instead of assert

--- test/ckuo_palindrome_test.rb
+++ test/ckuo_palindrome_test.rb
@@ -8,10 +8,19 @@ Minitest::Reporters.use!
 class CkuoPalindromeTest < Minitest::Test
 
   def test_non_palindrome
-    assert !"apple".palindrome?
+    refute "apple".palindrome?
   end
 
   def test_literal_palindrome
     assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    skip
+  end
+
 end
