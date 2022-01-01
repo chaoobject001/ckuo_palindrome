c9befe16f77e424bcb07131f529c55f56409582f
chaoobject001 <chao.kuo@gmail.com>
2021-12-31 19:31:52 -0800

    Restore the module in the palindrome gem

--- lib/ckuo_palindrome.rb
+++ lib/ckuo_palindrome.rb
@@ -2,41 +2,27 @@
 
 require_relative "ckuo_palindrome/version"
 
-class String
+module CkuoPalindrome
+  
   # Returns true for a palindrome, false otherwise.
   def palindrome?
-    processed_content == processed_content.reverse
-  end
-
-  # Returns the letters in the string.
-  def letters
-    # the_letters = []
-    # letter_regex = /[a-z]/i
-    
-    self.scan(/[a-z]/i).join.downcase
-
-    # self.chars.select { |c| c.match(/[a-z]/i)}.join
-
-    # self.chars.each do |character|
-    #   if character.match(letter_regex)
-    #     the_letters << character
-    #   end
-    # end
-
-    # for i in 0..self.length - 1 do
-    #   character = self[i] 
-    #   if character.match(letter_regex)
-    #     the_letters << self[i]
-    #   end
-    # end
-    # the_letters.join
+    if processed_content.empty?
+      false
+    else
+      processed_content == processed_content.reverse
+    end
   end
 
   private
     
     # Returns content for palindrome testing.
     def processed_content
-      self.letters.downcase
+      self.to_s.scan(/[a-z]/i).join.downcase
     end
+
+end
+
+class String
+  include CkuoPalindrome
 end
 
